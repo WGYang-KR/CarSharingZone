ea6f70b7991e19e572b7f816cac865dbb9fbaf82
양원국(WG.Yang) <72122503+WGYang-KR@users.noreply.github.com>
2022-06-02 16:39:20 +0900

    Xcode 프로젝트 생성

---
+++ SoCarApp/SoCarApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoCarApp
+//
+//  Created by WG Yang on 2022/06/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
