5e320af1f23a28303e04a633c01e1aa037a5879a
양원국(WG.Yang) <72122503+WGYang-KR@users.noreply.github.com>
2022-06-05 18:57:59 +0900

    FEAT: 메인화면 레이아웃

---
+++ SoCarApp/SoCarApp/MainPage/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  SoCarApp
+//
+//  Created by WG Yang on 2022/06/04.
+//
+
+import UIKit
+import MapKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBAction func touchUpInsideFavoriteZoneButton(_ sender: UIButton) {
+        
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
