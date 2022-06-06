73b1841114e5f530ded4540cdf1cedeefd96dc96
양원국(WG.Yang) <72122503+WGYang-KR@users.noreply.github.com>
2022-06-06 12:45:25 +0900

    FEAT: CarList 페이지 레이아웃

---
+++ SoCarApp/SoCarApp/CarListPage/CarListViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CarListViewController.swift
+//  SoCarApp
+//
+//  Created by WG Yang on 2022/06/06.
+//
+
+import UIKit
+
+class CarListViewController: UIViewController {
+
+    var selectedZone: Zone!
+    
+    @IBOutlet var zoneNameLabel: UILabel!
+    @IBOutlet var zoneAliasLabel: UILabel!
+    @IBOutlet var favoriteButton: UIButton!
+//    lazy var carList: [Car] = {
+//        
+//    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        //라벨 설정
+        
+        //즐겨찾기 버튼 설정
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
