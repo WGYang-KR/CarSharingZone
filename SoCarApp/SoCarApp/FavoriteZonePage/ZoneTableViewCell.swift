918aa9821369bdb1b000eccc14a083d24af60135
양원국(WG.Yang) <72122503+WGYang-KR@users.noreply.github.com>
2022-06-07 14:12:39 +0900

    FIX: 이미지 @2x,@3x 추가

---
+++ SoCarApp/SoCarApp/FavoriteZonePage/ZoneTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ZoneTableViewCell.swift
+//  SoCarApp
+//
+//  Created by WG Yang on 2022/06/07.
+//
+
+import UIKit
+
+class ZoneTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
