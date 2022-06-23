399e18adbb968a296f0301cbf022e2d70b17e355
양원국(WG.Yang) <72122503+WGYang-KR@users.noreply.github.com>
2022-06-24 04:01:53 +0900

    FIX: Rx 재설치

---
+++ SoCarApp/Pods/Target Support Files/Pods-CarSharingZone/Pods-CarSharingZone-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CarSharingZone : NSObject
+@end
+@implementation PodsDummy_Pods_CarSharingZone
+@end
--- SoCarApp/Pods/Target Support Files/Pods-CarSharingZone/Pods-CarSharingZone-umbrella.h
+++ SoCarApp/Pods/Target Support Files/Pods-CarSharingZone/Pods-CarSharingZone-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_SoCarAppVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SoCarAppVersionString[];
+FOUNDATION_EXPORT double Pods_CarSharingZoneVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CarSharingZoneVersionString[];
 
